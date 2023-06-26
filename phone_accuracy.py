191443abe5691e1402eac73ed1e1616fbeddb19a
Philip Combiths <philcombiths@gmail.com>
2023-06-25 19:21:27 -0500

    Update phone_accuracy.py
    Separate function phone_accuracy_by_participant added  for different column structure of this query.

--- phone_accuracy.py
+++ phone_accuracy.py
@@ -1,25 +1,36 @@
 """
 Created on Tue June 22, 2021
-Updated: 2023-02-11
-@author: Philip
+Updated: 2023-06-25
+@author: Philip Combiths
 
-Aggregates output from a phones query (e.g., Queries_v3_aggregate-accuracy.xml) in Phon 3.0+ (generate an aggregate 
-accuracy value for each unique phone/phone sequence. Useful for generating 
-output similar to Phon 2.2 accuracy queries from Phon 3.0 accuracy queries.
+Aggregate output from a phones query in Phon 3.0+. Specified for use with:
+- Queries_v3_aggregate-accuracy.xml 
+- Queries_v3_aggregate-accuracy.xml-by-participant 
+Use the appropriate function for the type of query output.
+
+Details: gGnerate an aggregate accuracy value for each unique phone/phone sequence. 
+Useful for generating utput similar to Phon 2.2 accuracy queries from Phon 3.0 
+phone queries.
 
 Uses test_func.py to batch run this process on multiple files in a folder and
-its subdirectories
+its subdirectories. User will be prompted for source directory.
 
-Use case:
-# Given a single file to convert. User will be asked to paste directory path 
-# containing file. 
-# Revised file will be created in same directory.
-
+Use cases:
+## Convert a single file:
 phone_accuracy(csv_to_pd()[0])
+# OR
+phone_accuracy_by_participant(csv_to_pd()[0])
 
-# Given a folder with subfolders of files to convert
+## Convert directory + subdirectories (original)
+result = test_func(phone_accuracy, csv_to_pd(overwrite_original=False))
 
-result = test_func(phone_accuracy, csv_to_pd())
+## Convert directory + subdirectories (by-participant query)
+result = test_func(phone_accuracy_by_participant, csv_to_pd(overwrite_original=False))
+
+## WARNING: deletes original files
+result = test_func(phone_accuracy, csv_to_pd(delete_original=True))
+## OR
+result = test_func(phone_accuracy_participant, csv_to_pd(delete_original=True))
 
 """
 import os
@@ -45,6 +56,8 @@ def change_dir(newdir):
 # Specify folder containing Phon output
 def folder_input(subdirectories=True, separate_file_path=True, path=False):
     """
+    Get file list from csv files in subdirectories.
+    Should work with by-participant query.
 
     Parameters
     ----------
@@ -88,7 +101,7 @@ def folder_input(subdirectories=True, separate_file_path=True, path=False):
 # By default gets folder of input folder_input()
 def csv_to_pd(fileList=folder_input(), delete_original=False, overwrite_original=False):
     """
-    Generates DataFrames from csv files in a directory.
+    Generate DataFrames from csv files in a directory.
 
     Parameters
     ----------
@@ -139,17 +152,20 @@ def csv_to_pd(fileList=folder_input(), delete_original=False, overwrite_original
                     elif overwrite_warning == "overwrite":
                         overwrite_accepted = True
                         print("Existing files overwritten.")
+        # For every remaining csv file:
         elif f[1].endswith(".csv"):
+            # Change to directory of file:
             with change_dir(f[0]):
+                # Read csv to dataframe df
                 df = pd.read_csv(f[1], encoding="utf-8")
-                # Add file directory path as a column so it stays with
-                # dataframe for later use
+                # Add file directory path as a column so it stays with df for later use
                 df["file_dir"] = f[0]
+                df["session"] = f[1].split(".")[0]  # Added for by-participant queries.
+                # Add df to df_list
                 df_list.append(df)
                 # WARNING: deletes original file
                 if delete_original:
                     os.remove(f[1])
-
     return df_list
 
 
@@ -157,7 +173,7 @@ def csv_to_pd(fileList=folder_input(), delete_original=False, overwrite_original
 # def df_preview(df):
 #     """
 
-#     Opens a pandas DataFrame in browser window.
+#     Open a pandas DataFrame in browser window. Requires dtale.
 
 #     Parameters
 #     ----------
@@ -169,10 +185,11 @@ def csv_to_pd(fileList=folder_input(), delete_original=False, overwrite_original
 #     return
 
 
-# Combine rows with same IPA Target
+# Combine rows with same IPA Target. Original aggregate_accuracy version.
 def phone_accuracy(df):
     """
-    Generates a new DataFrame with aggregated accuracy values.
+    Generates a new DataFrame with aggregated accuracy values
+    for Queries_v3_aggregate-accuracy.xml.
 
     Parameters
     ----------
@@ -200,7 +217,6 @@ def phone_accuracy(df):
     accuracy_df = accuracy_df.replace(np.nan, 0)
     accuracy_df["Accuracy"] = accuracy_df["Accurate"] / accuracy_df["Total"]
     accuracy_df["session"] = session_name
-
     dir_path = df["file_dir"].iloc[0]
     accuracy_df.to_csv(
         os.path.join(dir_path, session_name + "_Accuracy.csv"),
@@ -213,11 +229,65 @@ def phone_accuracy(df):
     return accuracy_df
 
 
-## Convert single file
-# phone_accuracy(csv_to_pd()[0])
+# Combine rows with same IPA Target. Revised version for query by particpant
+# with 'Alignment' column
+def phone_accuracy_by_participant(df):
+    """
+    Generate a new DataFrame with aggregated accuracy values
+    for Queries_v3_aggregate-accuracy.xml.
 
-## Convert directory + subdirectories
-result = test_func(phone_accuracy, csv_to_pd(overwrite_original=False))
+    Parameters
+    ----------
+    df : DATAFRAME
+        DataFrame from which to generate phone accuracy.
+
+    Returns
+    -------
+    accuracy_df : DATAFRAME
+        DataFrame with aggregated accuracy values by participant.
+
+    """
+    session_name = df["session"].iloc[0]  # Extract session name. Replaces line below
+    # session_name = df.columns[2]
+    df.rename(columns={"Unnamed: 3": "Total"}, inplace=True)  # Replaces line below
+    # df.rename(columns={session_name: "Total"}, inplace=True)
+    accurate_mask = df["IPA Target"] == df["IPA Actual"]
+    inaccurate_mask = df["IPA Target"] != df["IPA Actual"]
+
+    accuracy_df = df.groupby(df["IPA Target"]).sum()
+    accurate_df = df[accurate_mask].rename(columns={"Total": "Accurate"})
+    inaccurate_df = df[inaccurate_mask].rename(columns={"Total": "Inaccurate"})
+    accurate_df = accurate_df.groupby(accurate_df["IPA Target"]).sum()
+    inaccurate_df = inaccurate_df.groupby(inaccurate_df["IPA Target"]).sum()
+
+    accuracy_df = pd.concat([accurate_df, inaccurate_df, accuracy_df], axis=1)
+    accuracy_df = accuracy_df.replace(np.nan, 0)
+    accuracy_df["Accuracy"] = accuracy_df["Accurate"] / accuracy_df["Total"]
+    accuracy_df["session"] = session_name
+
+    dir_path = df["file_dir"].iloc[0]
+    accuracy_df.to_csv(
+        os.path.join(dir_path, session_name + "_Accuracy.csv"),
+        columns=["Total", "Accurate", "Inaccurate", "session"],
+        encoding="utf-8",
+        index=True,
+    )
+    return accuracy_df
+
+
+### Script Executions
+## Convert a single file:
+# phone_accuracy(csv_to_pd()[0])
+# OR
+# phone_accuracy_by_participant(csv_to_pd()[0])
+
+## Convert directory + subdirectories (original)
+# result = test_func(phone_accuracy, csv_to_pd(overwrite_original=False))
+
+## Convert directory + subdirectories (by-participant query)
+result = test_func(phone_accuracy_by_participant, csv_to_pd(overwrite_original=False))
 
 ## WARNING: deletes original files
 # result = test_func(phone_accuracy, csv_to_pd(delete_original=True))
+## OR
+# result = test_func(phone_accuracy_participant, csv_to_pd(delete_original=True))
